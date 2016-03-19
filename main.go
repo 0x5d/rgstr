34adab8e56c6670c78d9ad63f2c557158478225d
David Castillo <castillobgr@gmail.com>
2016-03-19 15:00:15 -0500

    Simplified logic.

--- main.go
+++ main.go
@@ -17,9 +17,10 @@ var delay *int
 var pods = make(map[string]*v1alpha.Pod)
 
 func main() {
+	address := flag.String("a", "localhost:15441", "The address where rkt's API service is listening.")
 	delay = flag.Int("d", 100, "The polling interval (in milliseconds).")
 	flag.Parse()
-	conn, err := grpc.Dial("localhost:15441", grpc.WithInsecure())
+	conn, err := grpc.Dial(*address, grpc.WithInsecure())
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -27,57 +28,33 @@ func main() {
 	c := v1alpha.NewPublicAPIClient(conn)
 	defer conn.Close()
 
-	if err = initPods(c); err != nil {
-		panic(err)
-	}
 	done := make(chan bool)
 	go startPolling(c, done)
 	<-done
 }
 
-func initPods(c v1alpha.PublicAPIClient) error {
-	res, err := getRunningAndExitedPods(c)
-	if err != nil {
-		return err
-	}
-	for _, p := range res.Pods {
-		if p.State == v1alpha.PodState_POD_STATE_RUNNING {
-			// TODO: register service.
-		}
-		pods[p.Id] = p
-	}
-	return nil
-}
-
 func startPolling(c v1alpha.PublicAPIClient, done chan bool) {
 	for {
-		res, err := getRunningAndExitedPods(c)
+		res, err := getRunningPods(c)
 		if err != nil {
 			panic(err)
 		}
 
 		for _, pod := range res.Pods {
-			p, ok := pods[pod.Id]
+			_, ok := pods[pod.Id]
 			if !ok {
-				// Pod wasn't mapped. Map it.
-				pods[pod.Id] = pod
 				// Check if it's running.
 				if pod.State == v1alpha.PodState_POD_STATE_RUNNING {
+					// Map the pod.
+					pods[pod.Id] = pod
 					// TODO: register it.
 				}
 				continue
 			}
-			if pod.State == p.State {
-				// No changes.
-				continue
-			}
 
-			if pod.State == v1alpha.PodState_POD_STATE_RUNNING {
-				pods[pod.Id] = pod
-				// TODO: Pod started running. Register it.
-			} else {
-				// TODO: Deregister it.
-				pods[pod.Id] = pod
+			if pod.State == v1alpha.PodState_POD_STATE_EXITED {
+				// TODO: Pod stopped. Deregister it.
+				delete(pods, pod.Id)
 			}
 		}
 		time.Sleep(time.Duration(*delay) * time.Millisecond)
@@ -85,16 +62,13 @@ func startPolling(c v1alpha.PublicAPIClient, done chan bool) {
 	done <- true
 }
 
-func getRunningAndExitedPods(c v1alpha.PublicAPIClient) (*v1alpha.ListPodsResponse, error) {
+func getRunningPods(c v1alpha.PublicAPIClient) (*v1alpha.ListPodsResponse, error) {
 	req := &v1alpha.ListPodsRequest{
 		// Specify the request: Fetch and print only running pods and their details.
 		Detail: true,
 		Filters: []*v1alpha.PodFilter{
 			{
-				States: []v1alpha.PodState{
-					v1alpha.PodState_POD_STATE_RUNNING,
-					v1alpha.PodState_POD_STATE_EXITED,
-				},
+				States: []v1alpha.PodState{v1alpha.PodState_POD_STATE_RUNNING},
 			},
 		},
 	}
