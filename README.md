a0877afcc56ebbc869c2b14bc7dea5b6d0bd381f
David Castillo <castillobgr@gmail.com>
2016-03-19 14:20:31 -0500

    First commit.
    Implemented a basic Proof of concept using rkt's API service.Updated README. Added a gitignore.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # rgstr
+**rgstr** (_re-gis-ter_) automatically registers and de-registers `rkt` pods on [Consul](), [etcd]().
+
+## Please Note:
+This project relies heavily on
+[`rkt`'s API service](https://github.com/coreos/rkt/blob/master/Documentation/subcommands/api-service.md),
+which is currently in an experimental phase. Until the API reaches stability, this project is not
+suitable for a production environment.
+
+
+# Run it
+Build **rgstr**:
+```sh
+go build
+```
+Run rkt's API service:
+```sh
+rkt api-service
+```
+
+Run Consul
+
+Run **rgstr**:
+```sh
+./rgstr
+```
+
+## Inspiration
+**rgstr** was inspired by [progrium](https://github.com/progrium)'s and
+[gliderlabs](https://github.com/gliderlabs)'s [registrator](https://github.com/gliderlabs/registrator).
+
+## License
+MIT
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"google.golang.org/grpc"
+
+	"github.com/coreos/rkt/api/v1alpha"
+	"github.com/davecgh/go-spew/spew"
+)
+
+var delay *int
+var pods = make(map[string]*v1alpha.Pod)
+
+func main() {
+	delay = flag.Int("d", 100, "The polling interval (in milliseconds).")
+	flag.Parsed()
+	conn, err := grpc.Dial("localhost:15441", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	c := v1alpha.NewPublicAPIClient(conn)
+	defer conn.Close()
+
+	if err = initPods(c); err != nil {
+		panic(err)
+	}
+	done := make(chan bool)
+	go startPolling(c, done)
+	<-done
+}
+
+func initPods(c v1alpha.PublicAPIClient) error {
+	res, err := getRunningAndExitedPods(c)
+	if err != nil {
+		return err
+	}
+	for _, p := range res.Pods {
+		if p.State == v1alpha.PodState_POD_STATE_RUNNING {
+			// TODO: register service.
+		}
+		pods[p.Id] = p
+	}
+	spew.Dump(pods)
+	return nil
+}
+
+func startPolling(c v1alpha.PublicAPIClient, done chan bool) {
+	for {
+		res, err := getRunningAndExitedPods(c)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, pod := range res.Pods {
+			p, ok := pods[pod.Id]
+			if !ok {
+				// Pod wasn't mapped. Map it.
+				pods[pod.Id] = pod
+				// Check if it's running.
+				if pod.State == v1alpha.PodState_POD_STATE_RUNNING {
+					// TODO: register it.
+				}
+				continue
+			}
+			if pod.State == p.State {
+				// No changes.
+				continue
+			}
+
+			if pod.State == v1alpha.PodState_POD_STATE_RUNNING {
+				pods[pod.Id] = pod
+				// TODO: Pod started running. Register it.
+			} else {
+				// TODO: Deregister it.
+				pods[pod.Id] = pod
+			}
+		}
+		time.Sleep(time.Duration(*delay) * time.Millisecond)
+	}
+	done <- true
+}
+
+func getRunningAndExitedPods(c v1alpha.PublicAPIClient) (*v1alpha.ListPodsResponse, error) {
+	req := &v1alpha.ListPodsRequest{
+		// Specify the request: Fetch and print only running pods and their details.
+		Detail: true,
+		Filters: []*v1alpha.PodFilter{
+			{
+				States: []v1alpha.PodState{
+					v1alpha.PodState_POD_STATE_RUNNING,
+					v1alpha.PodState_POD_STATE_EXITED,
+				},
+			},
+		},
+	}
+	return c.ListPods(context.Background(), req)
+}
